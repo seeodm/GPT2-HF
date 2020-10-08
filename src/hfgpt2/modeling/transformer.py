1a71614caf62b24c5b65ef0d3c1881cac1d78b42
seeodm <sdm0325@gmail.com>
2020-10-08 18:02:19 +0900

    [master] custom GPT2LMHeadModel

--- src/hfgpt2/modeling/transformer.py
+++ src/hfgpt2/modeling/transformer.py
@@ -9,48 +9,11 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
         self.transformer = GPT2Model(config)
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-        self.use_return_dict = False
         self.init_weights()
 
-    def forward(
-        self,
-        input_ids=None,
-        past_key_values=None,
-        attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        head_mask=None,
-        inputs_embeds=None,
-        encoder_hidden_states=None,
-        encoder_attention_mask=None,
-        labels=None,
-        use_cache=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-        **kwargs,
-    ):
-        if "past" in kwargs:
-            past_key_values = kwargs.pop("past")
-        assert kwargs == {
-        }, f"Unexpected keyword arguments: {list(kwargs.keys())}."
-        return_dict = return_dict if return_dict is not None else self.use_return_dict
+    def forward(self, input_ids):
+        transformer_outputs = self.transformer(input_ids)
 
-        transformer_outputs = self.transformer(
-            input_ids,
-            past_key_values=past_key_values,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds,
-            encoder_hidden_states=encoder_hidden_states,
-            encoder_attention_mask=encoder_attention_mask,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
         hidden_states = transformer_outputs[0]
 
         lm_logits = self.lm_head(hidden_states)
